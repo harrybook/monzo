31e0aea0c91304a6ce2a7799037ad7f1c3dce2ad
David Gauld <dgauld@google.com>
2016-09-11 15:09:59 +0100

    Add configuration files

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+  api: {
+    url: 'http://hello.local:6061/1.0',
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+const config = {
+  entry: './src/js/app.js',
+  output: {
+    path: './dist/js',
+    filename: 'app.js',
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  stats: {
+    children: false,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  vue: {
+    loaders: {
+      js: 'babel!eslint?{rules:{\'eol-last\':0}}',
+      scss: ExtractTextPlugin.extract('css!sass'),
+    },
+    autoprefixer: true,
+  },
+  plugins: [
+    new ExtractTextPlugin('../css/app.css'),
+  ],
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"production"',
+    },
+  }));
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+    },
+  }));
+} else {
+  config.devtool = '#source-map';
+}
+
+module.exports = config;
