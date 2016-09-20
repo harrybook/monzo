8cb95c79f22f91f56573ab386020a2eab5b3f964
David Gauld <dgauld@google.com>
2016-09-20 16:15:30 +0100

    Add simple login link

--- src/vue/pages/Home.vue
+++ src/vue/pages/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="c-home">
-    <h1>Home</h1>
+    <a :href="'https://auth.getmondo.co.uk/?client_id=' + config.client.id + '&redirect_uri=' + config.client.redirectUri + '&response_type=code'">Login with Monzo</a>
   </div>
 </template>
+
+<script>
+import config from '../../../config';
+
+export default {
+  data() {
+    return {
+      config,
+    };
+  },
+}
+</script>
