cb21c02181baab114138dae11f9b6a0bab8ad028
David Gauld <dgauld@google.com>
2016-10-03 20:33:30 +0100

    Fix error within Vuex actions

--- src/js/vuex/actions.js
+++ src/js/vuex/actions.js
@@ -6,7 +6,7 @@ export function retrieveAccounts({ commit }) {
   commit('accountsRetrieve');
   api.retrieveAccounts()
     .then(response => {
-      commit('accountsReceive', response.json().accounts);
+      commit('accountsReceive', response.body.accounts);
     })
     .catch(response => {
       // commit('accountsError', response.data.error.message);
@@ -17,7 +17,7 @@ export function retrieveBalance({ commit }, accountId) {
   commit('balanceRetrieve');
   api.retrieveBalance(accountId)
     .then(response => {
-      commit('balanceReceive', response.json());
+      commit('balanceReceive', response.body);
     })
     .catch(response => {
       // commit('balanceError', response.data.error.message);
@@ -28,7 +28,7 @@ export function retrieveTransactions({ commit }, accountId) {
   commit('transactionsRetrieve');
   api.retrieveTransactions(accountId)
     .then(response => {
-      commit('transactionsReceive', response.json().transactions);
+      commit('transactionsReceive', response.body.transactions);
     })
     .catch(response => {
       // commit('transactionsError', response.data.error.message);
@@ -39,7 +39,7 @@ export function retrieveTransaction({ commit }, transactionId) {
   commit('transactionRetrieve');
   api.retrieveTransaction(transactionId)
     .then(response => {
-      commit('transactionReceive', response.json().transaction);
+      commit('transactionReceive', response.body.transaction);
     })
     .catch(response => {
       // commit('transactionError', response.data.error.message);
