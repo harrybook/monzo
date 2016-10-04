6a20db2bbc4c76cc64f0ef6d1cf0abbf0e9e6e7c
David Gauld <dgauld@google.com>
2016-10-05 00:51:09 +0100

    Add config.sample.js file

---
+++ config.sample.js
@@ -0,0 +1,10 @@
+module.exports = {
+  client: {
+    id: 'YOUR_CLIENT_ID',
+    secret: 'YOUR_CLIENT_SECRET',
+    redirectUri: 'http://localhost:2020/callback',
+  },
+  googleMaps: {
+    key: 'YOUR_GOOGLE_MAPS_KEY',
+  },
+};
